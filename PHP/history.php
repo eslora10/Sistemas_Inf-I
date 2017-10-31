18f1fc856bd8803fd3f30ead9407f01037653c63
Antonio Amor <antonio.amor.mourelle@gmail.com>
2017-10-31 16:00:51 +0100

    ya desplegando, anyadir cambio saldo

--- PHP/history.php
+++ PHP/history.php
@@ -14,25 +14,24 @@ session_start();
                           <h2>Historial</h2>
 
                       <?php
-                      $total = 0;
                       $historial = simplexml_load_file("../XML/historial.xml");
                       echo "<table class=\"center\">";
                       echo "<tr>";
-                      echo "<th colspan=\"2\">Fecha </th>";
+                      echo "<th >Fecha </th>";
                       echo "</tr>";
                       foreach ($historial->Fecha as $day){
                         echo "<tr >"; /* ponemos la clase desplegable aqui para que la funcion .next de JQUERY  funcione ; necesita un sibbling*/
-                        echo "<td><a class=\"desplegar\">$day->date</a></td>";
+                        echo "<td>$day->date <a class=\"desplegar\"><i class='fa fa-caret-square-o-down' aria-hidden='true'></i></a></td>";
                         echo "</tr>";
 
                         #dentro del tr generamos la tabla nueva
                         echo "<tr class=\"prueba\">";
-                        echo "<td colspan=\"2\">";
-                          echo "<table class=\"center\">";
+                        echo "<td >";
+                          echo "<table class='minicenter'>";
                           $catalogo = simplexml_load_file("../XML/catalogo.xml");
                           foreach ($day->pelicula as $p) {
 
-                            $titulo = $catalogo->xpath("/catalogo/pelicula[id=\"$p->id\"]/titulo");
+                            $titulo = $catalogo->xpath("/catalogo/pelicula[id=\"$p->id\"]/titulo")[0];
 
                             echo "<tr>";
                             echo "<td >$titulo</td>";
@@ -46,60 +45,12 @@ session_start();
                       echo "</table>";
                       ?>
 
-                      <?php
-
-                     /* echo "<table class=\"center\">";
-                      echo "<tr>";
-                      echo "<th colspan=\"2\">Fecha </th>";
-                      echo "</tr>";
-                      echo "<tr>";
-                      echo "<td>hola</td>";
-                      echo "<td>adios</td>";
-                      echo "</tr>";
-
-
-                      echo "<tr>";
-                          echo "<table class=\"center\">";
-                          echo "<tr>";
-                          echo "<th colspan=\"2\">Fecha </th>";
-                          echo "</tr>";
-                          echo "<tr>";
-                          echo "<td>hola</td>";
-                          echo "<td>adios</td>";
-                          echo "</tr>";
-
-                          echo "<tr>";
-                              echo "<table class=\"center\">";
-                              echo "<tr>";
-                              echo "<th colspan=\"2\">Fecha </th>";
-                              echo "</tr>";
-                              echo "<tr>";
-                              echo "<td>hola</td>";
-                              echo "<td>adios</td>";
-                              echo "</tr>";
-                              echo "</table>";
-                          echo "</tr>";
-
-
-                          echo "</table>";
-                      echo "</tr>";
-
-                      echo "</table>";
-*/
-                      ?>
-
-
-                  <h3 class="basket-total"> Total: <?php echo $total; ?>€ </h3>
+                  <h3 class="basket-total"> Saldo disponible: <?php echo $_SESSION['saldo']; ?>€ </h3>
                   <a class="login" href="index-logged.html">Confirmar</a>
                       </div>
                   </div>
           </div>
         </div>
         <footer>Antonio Amor, Esther López, Sistemas Informáticos</footer>
-
-
-        <script>
-        $( ".desplegar" ).closest("tr").nextAll( ".prueba" ).css('background', 'yellow');
-        </script>
 
 </body></html>
