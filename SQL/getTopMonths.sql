46a6e11b30dd46a9c5518195367a2bca3a59a30f
Antonio Amor <antonio.amor.mourelle@gmail.com>
2017-11-16 18:04:47 +0100

    getTopMonth a medias

---
+++ SQL/getTopMonths.sql
@@ -0,0 +1,22 @@
+﻿DROP function getTopMonths(INTEGER);
+CREATE OR REPLACE FUNCTION getTopMonths(integer) RETURNS TABLE( ymf text) AS $$
+DECLARE
+
+BEGIN
+	RETURN QUERY SELECT YM 
+	FROM
+		(SELECT TO_CHAR(orderdate, 'YYYY-MM') AS YM, sum(totalamount) AS TAYM FROM orders
+		GROUP BY TO_CHAR(orderdate, 'YYYY-MM')
+		ORDER BY TO_CHAR(orderdate, 'YYYY-MM')) AS totAmYM 
+		NATURAL JOIN
+		--products!!!!!!!!
+		(SELECT TO_CHAR(orderdate, 'YYYY-MM') AS YM, sum(totalamount) AS TAYM FROM orders
+		GROUP BY TO_CHAR(orderdate, 'YYYY-MM')
+		ORDER BY TO_CHAR(orderdate, 'YYYY-MM')) AS totAmYM
+	WHERE TAYM > $1
+	ORDER BY YM DESC;
+	
+END;
+$$ LANGUAGE 'plpgsql';
+
+select getTopMonths(1);
