a73f8d0e98d7209d25ccb2834e493501dbd0906d
antonio <antonio.amor@estudiante.uam.es>
2017-11-22 23:07:07 +0100

    ya suma y resta, falta añadir alertas

--- SQL/updInventory.sql
+++ SQL/updInventory.sql
@@ -1,19 +1,49 @@
-﻿DROP FUNCTION IF EXISTS updateInventory()
-CREATE FUNCTION updateInventory() RETURNS TRIGGER AS $$
+﻿--DROP FUNCTION IF EXISTS updateInventory();
+
+CREATE OR REPLACE FUNCTION updateInventory() 
+RETURNS TRIGGER AS $$
 DECLARE
-	oID record;
-
+	res record;
+	comp integer;
 BEGIN
+FOR res in (select prod_id, sum(quantity) as sum
+		from orderdetail Natural Join orders 
+		where NEW.orderid =orderdetail.orderid 
+		group by prod_id)
 
-UPDATE inventory SET
-stock=stock -1
-sales=sales+1
-Where invent.prod_id in (select distinct prod_id from orderdetails Natural Join orders where NEW.orderid =orderdetail.orderid)
+LOOP
+	UPDATE inventory SET
+--	comp stock - res.sum
+	
+	sales= sales + res.sum,
+	stock= stock - res.sum
+	Where prod_id = res.prod_id;
+
+--	IF comp<=0 THEN
+--	INSERT into alertas VALUES(res.prod_id, "producto agotado");
+--	END IF;
+
+END LOOP;
+
+RETURN NEW; 
 END;
-$$LANGUAGE 'plpgsql'
+$$LANGUAGE 'plpgsql';
 
-UPDATE OR CREATE TRIGGER t_updateInventory() AFTER UPDATE status ON orders
-FOR EACH ROW EXECUTE PROCEDURE updateInventory();
+DROP TRIGGER IF EXISTS t_updateInventory ON orders;
+CREATE TRIGGER t_updateInventory AFTER UPDATE ON orders
+FOR EACH ROW 
+WHEN (OLD.status IS DISTINCT FROM NEW.status)
+EXECUTE PROCEDURE updateInventory();
 
 
-OLD.
+
+--SELECT * from inventory where prod_id in(select prod_id as sum
+--		from orderdetail Natural Join orders 
+--		where 1 =orderdetail.orderid 
+--		group by prod_id);
+--UPDATE orders SET status = 2 WHERE orderid = 1;
+
+--UPDATE orderdetail SET quantity=1 where prod_id=1014;
+
+--UPDATE inventory SET stock = 2 where prod_id=1014
+
