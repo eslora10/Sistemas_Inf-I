c70f009cb322ceab10f01f1eefdf1912ddd8d8ec
Antonio Amor <antonio.amor.mourelle@gmail.com>
2017-11-22 18:07:22 +0100

    getMonths in progress

--- SQL/getTopMonths.sql
+++ SQL/getTopMonths.sql
@@ -1,4 +1,4 @@
-﻿DROP function getTopMonths(INTEGER);
+﻿delete if exist function getTopMonths(INTEGER);
 CREATE OR REPLACE FUNCTION getTopMonths(integer) RETURNS TABLE( ymf text) AS $$
 DECLARE
 
@@ -7,16 +7,21 @@ BEGIN
 	FROM
 		(SELECT TO_CHAR(orderdate, 'YYYY-MM') AS YM, sum(totalamount) AS TAYM FROM orders
 		GROUP BY TO_CHAR(orderdate, 'YYYY-MM')
-		ORDER BY TO_CHAR(orderdate, 'YYYY-MM')) AS totAmYM 
-		NATURAL JOIN
-		--products!!!!!!!!
-		(SELECT TO_CHAR(orderdate, 'YYYY-MM') AS YM, sum(totalamount) AS TAYM FROM orders
-		GROUP BY TO_CHAR(orderdate, 'YYYY-MM')
+		Where TAYM > $1
 		ORDER BY TO_CHAR(orderdate, 'YYYY-MM')) AS totAmYM
+		--NATURAL JOIN
+		Select TO_CHAR(orderdate, 'YYYY-MM')
+		From	
+			(SELECT orderid, sum(quantity) as Nprod FROM orderdetail
+			Group by orderid) as suma
+			Natural join
+			(Select orderdate From orders) as prod
+		WHERE Nprod >$2
+		
 	WHERE TAYM > $1
 	ORDER BY YM DESC;
 	
 END;
 $$ LANGUAGE 'plpgsql';
 
-select getTopMonths(1);
+select getTopMonths(320000, 2);
