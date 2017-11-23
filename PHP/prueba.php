3c372c3e0111f27ebc94873f3d2e3135e646cccd
Antonio Amor <antonio.amor.mourelle@gmail.com>
2017-11-23 18:05:31 +0100

    prueba con cosas de pdo

--- PHP/prueba.php
+++ PHP/prueba.php
@@ -1,5 +1,42 @@
 <?php
-
-$URL="http://localhost/Sistemas_Inf-I/PHP/index.php/";
-echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+session_start();
 ?>
+<!DOCTYPE html>
+<html lang="es">
+    <?php include("includeHead.php") ?>
+    <body>
+      <?php include("includeHeader.php") ?>
+        <div class="container-fluid">
+            <div class="main-content">
+                <?php
+
+                try {
+                $db = new PDO("pgsql:dbname=si1; host=localhost", "alumnodb", "alumnodb" );
+                /*** use the database connection ***/
+                }
+                catch(PDOException $e){
+                echo $e->getMessage();
+                }
+
+
+                $sql = "SELECT * FROM customers where username='shad'";
+
+                foreach ($db->query($sql) as $row)
+                {
+                  echo '<h1>';
+                  print $row['username'] . '<br />';
+                  echo '</h1>';
+                }
+
+
+
+                ?>
+
+              </div>
+
+
+          </div>
+          <?php include("includeFooter.php") ?>
+
+
+  </body></html>
